c3b8be5e41d21d612b3ef4a437a3430de97cc589
Ivan <popov1703.ivan@ya.ru>
2022-04-30 23:38:31 +0300

    Table

--- src/pages/MyTest.jsx
+++ src/pages/MyTest.jsx
@@ -1,12 +1,50 @@
 import React from 'react';
 import '../styles/MyTest.css'
+import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table';
+import * as PropTypes from "prop-types";
 
+function Row(props) {
+    return null;
+}
+
+Row.propTypes = {
+    className: PropTypes.string,
+    children: PropTypes.node
+};
 const MyTest = (props) => {
+
+    let products = [{
+        id: 2,
+        name: "Item name 1",
+        price: 100
+    },{
+        id: 1,
+        name: "Item name 2",
+        price: 100
+    },{
+        id: 3,
+        name: "Item name 3",
+        price: 200
+    }];
+
+    function priceFormatter(cell, row){
+        return '<i class="glyphicon glyphicon-usd"></i> ' + cell;
+    }
+
+
     return (
         <div className="my-test-page">
             <button className="btn btn-primary new-test">Создать новый тест</button>
             <div className="content">
-
+                <BootstrapTable
+                    data={products}
+                    striped={true}
+                    hover={true}
+                >
+                    <TableHeaderColumn dataField="id" isKey={true} dataAlign="center" dataSort={true}>Название теста</TableHeaderColumn>
+                    <TableHeaderColumn dataField="name" dataSort={true}>Дата создания</TableHeaderColumn>
+                    <TableHeaderColumn dataField="price" dataFormat={priceFormatter}>Product Price</TableHeaderColumn>
+                </BootstrapTable>
             </div>
         </div>
     );
